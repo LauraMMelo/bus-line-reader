20104c97ea1343977cb695b138c23e2a2e3b1531
felipelucio <felipelucio2009@gmail.com>
2019-06-04 17:11:24 -0300

    Modificação do arquivo busLineReader.py, para permitir a leitura do da imagem usando apenas opencv. O problema de execução anterior foi resolvido com a configuração do ambiente de desenvolvimento, entretanto pytesseract ainda não consegue ler a placa do ônibus.

--- busLineReader.py
+++ busLineReader.py
@@ -1,20 +1,19 @@
 import cv2
-import numpy as np
 import pytesseract as pyOcr
 from PIL import Image
 
-img = Image.open('papicu.jpg').convert('RGB')
-arrayImg = np.asarray(img).astype(np.uint8)
-arrayImg[:, :, 0] = 0 # zerar o canal RED 
-arrayImg[:, :, 2] = 0 # zerar o canal BLUE
+img = cv2.imread('papicu.jpg', 0)
 
-imCv = cv2.cvtColor(arrayImg, cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(imCv, 127, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+cv2.imshow('ImagemP&B', img)
 
-binaryImg = Image.fromarray(thresh)
+ret, threshImg = cv2.threshold(img, 150, 255, cv2.THRESH_BINARY)
+
+cv2.imshow('ImagemBinarizada', threshImg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+binaryImg = Image.fromarray(threshImg)
 linha = pyOcr.image_to_string(binaryImg, lang='por')
-
-cv2.waitkey(0)
 
 print(linha)
 
